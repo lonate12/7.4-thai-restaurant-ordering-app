a0cfd0b0c73a96b9f632dbbb150d1cf5806274fa
Luis Rene Onate <lonate12@gmail.com>
2016-10-28 19:42:57 -0400

    Began working on components.

---
+++ app/scripts/components/order.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var menuItemModel = require('../models/menu-items.js').MenuItem;
+var menuItemCollection = require('../models/menu-items.js').MenuItemCollection;
+
+var MenuList = React.createClass({
+  render: function(){
+    return(
+      <div class="col-md-9 menu-container">
+        
+      </div>
+    );
+  }
+});
